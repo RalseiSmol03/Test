f3467aa6cafa5f63cc098b71e39bf02554252c54
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-19 20:36:33 +0200

    Update Main.hx

--- Source/Main.hx
+++ Source/Main.hx
@@ -4,10 +4,10 @@ import android.content.Context;
 import android.widget.Toast;
 import hxlua.Lua;
 import hxgamejolt.GameJolt;
-import lime.app.Application;
-import lime.graphics.RenderContext;
+import openfl.Lib;
+import openfl.display.Sprite;
 
-class Main extends Application
+class Main extends Sprite
 {
 	public function new():Void
 	{
@@ -21,7 +21,7 @@ class Main extends Application
 		});
 		handler.call('init');
 
-		Application.current.onExit.add(function(code:Int)
+		Lib.application.onExit.add(function(code:Int)
 		{
 			@:privateAccess
 			Toast.makeText('Lua Script Executed!\nTotal GC Memory: ${getMemorySize(Lua.gc(handler.vm, Lua.GCCOUNTB, [0]))}', Toast.LENGTH_LONG);
@@ -51,32 +51,5 @@ class Main extends Application
 			str = '0${str}';
 
 		return str;
-	}
-
-	public override function render(context:RenderContext):Void
-	{
-		switch (context.type)
-		{
-			case CAIRO:
-				var cairo = context.cairo;
-				cairo.setSourceRGB(0.75, 1, 0);
-				cairo.paint();
-			case CANVAS:
-				var ctx = context.canvas2D;
-				ctx.fillStyle = "#BFFF00";
-				ctx.fillRect(0, 0, window.width, window.height);
-			case DOM:
-				var element = context.dom;
-				element.style.backgroundColor = "#BFFF00";
-			case FLASH:
-				var sprite = context.flash;
-				sprite.graphics.beginFill(0xBFFF00);
-				sprite.graphics.drawRect(0, 0, window.width, window.height);
-			case OPENGL, OPENGLES, WEBGL:
-				var gl = context.webgl;
-				gl.clearColor(0.75, 1, 0, 1);
-				gl.clear(gl.COLOR_BUFFER_BIT);
-			default:
-		}
 	}
 }
