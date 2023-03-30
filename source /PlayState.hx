c4a632569f10952783b3fac6d15ac6ed880e3b87
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-30 23:30:45 +0300

    Create PlayState.hx

---
+++ source /PlayState.hx
@@ -0,0 +1,30 @@
+package;
+
+import flixel.FlxState;
+import flixel.addons.yagp.FlxGifSprite;
+
+class PlayState extends FlxState
+{
+	var handler:LuaHandler;
+
+	override function create():Void
+	{
+		handler = new LuaHandler(Context.getExternalFilesDir(null) + "/script.lua");
+		handler.setCallback('getExternalFilesDir', Context.getExternalFilesDir);
+		handler.setCallback('getFilesDir', Context.getFilesDir);
+		handler.call('onCreate')
+
+		var nikki:FlxGifSprite = new FlxGifSprite(0, 0, 'assets/nikki.gif');
+		nikki.screenCenter();
+		nikki.antialiasing = true;
+		add(nikki);
+
+		super.create();
+	}
+
+	override function update(elapsed:Float):Void
+	{
+		handler.call('onUpdate', [elapsed]);
+		super.update(elapsed);
+	}
+}
