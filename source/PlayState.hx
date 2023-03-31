70de0a623b495e84bf1b07eb31fcd9a379b53578
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-31 10:38:59 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,15 @@
 package;
 
 import android.content.Context;
+import flixel.FlxG;
 import flixel.FlxState;
 import flixel.addons.yagp.FlxGifSprite;
+import openfl.filters.ShaderFilter;
 
 class PlayState extends FlxState
 {
 	var handler:LuaHandler;
+	var chrome:ShaderFilter;
 
 	override function create():Void
 	{
@@ -15,9 +18,12 @@ class PlayState extends FlxState
 		handler.setCallback('getFilesDir', Context.getFilesDir);
 		handler.call('onCreate');
 
+		chrome = new ShaderFilter(new Chrome());
+
 		var nikki:FlxGifSprite = new FlxGifSprite(0, 0, 'assets/nikki.gif');
 		nikki.screenCenter();
 		nikki.antialiasing = true;
+		nikki.shader = chrome;
 		add(nikki);
 
 		super.create();
@@ -26,6 +32,10 @@ class PlayState extends FlxState
 	override function update(elapsed:Float):Void
 	{
 		handler.call('onUpdate', [elapsed]);
+
+		if (chrome != null)
+			chrome.shader.data.bOffset.value = [FlxG.random.float(-10, 10) / 1000];
+
 		super.update(elapsed);
 	}
 }
