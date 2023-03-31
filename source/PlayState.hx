079338f41ffd23b294e11f74df60ddf65d714f33
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-03-31 14:46:35 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -24,7 +24,7 @@ class PlayState extends FlxState
 		add(bg);
 
 		legion = new FlxGifSprite(0, 0, 'assets/Legion.gif');
-		// legion.setGraphicSize(Std.int(legion.width * 1.1), Std.int(legion.height * 1.1));
+		legion.setGraphicSize(Std.int(legion.width * 0.87), Std.int(legion.height * 0.87));
 		legion.screenCenter();
 		legion.shader = new Chrome();
 		add(legion);
