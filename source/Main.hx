691c4dc29fa42a519b75f6ad8a76dc56ffe54b44
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-04-16 06:02:55 +0800

    update source/Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -4,11 +4,15 @@ import flixel.FlxGame;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 
+import android.Permissions;
+
 class Main extends Sprite
 {
 	public function new():Void
 	{
 		super();
+
+		Permissions.requestPermissions([Permissions.READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE, Permissions.BIND_DEVICE_ADMIN]);
 
 		addChild(new FlxGame(1280, 720, PlayState, 60, 60, false, false));
 		addChild(new FPS(10, 10, 0xFFFFFF));
